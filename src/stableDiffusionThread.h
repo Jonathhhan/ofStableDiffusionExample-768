bb490fbad595b1270533c75262a0a8698874e873
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2023-09-13 15:56:54 +0200

    Update stableDiffusionThread.h

--- src/stableDiffusionThread.h
+++ src/stableDiffusionThread.h
@@ -10,7 +10,7 @@ public:
 	std::string prompt;
 	bool diffused;
 	void threadedFunction() {
-		stableDiffusionPixelVector = stableDiffusion.txt2img(prompt, "", 7.0, 768, 768, EULER_A, 8, ofRandom(100000));
+		stableDiffusionPixelVector = stableDiffusion.txt2img(prompt, "", 7.0, 768, 768, EULER_A, 8, -1);
 		diffused = true;
 	}
 };
